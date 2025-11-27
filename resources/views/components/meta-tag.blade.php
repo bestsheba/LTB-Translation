50552432c185ceb6d2c6ddf520e916d94f60acd3
devzkhalil <devzkhalil@gmail.com>
2025-11-27 21:32:24 +0600

    fav added

--- resources/views/components/meta-tag.blade.php
+++ resources/views/components/meta-tag.blade.php
@@ -4,7 +4,11 @@
 
 <title>@yield('title') - {{ config('app.name', 'LTB Translation') }}</title>
 
-<link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+<link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+<link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
